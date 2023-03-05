a35be077048437c8fdbacec8638d3a2fe89aaba5
dezet36я <sadkovsky36@gmail.com>
2023-03-05 14:51:43 +0300

    v0.2

--- src/components/BudgetCard/style.ts
+++ src/components/BudgetCard/style.ts
@@ -1,9 +1,14 @@
 import styled from "styled-components";
 
 const StyleBudgetCard = styled.div`
-  padding: 38px 20px;
-  background: #7cc6fe;
-  border-radius: 20px;
+  padding: 0 20px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  gap: 60px;
+  height: 100px;
+  border-radius: 10px;
+  background-color: #7cc6fe;
 `;
 
 const StyleText = styled.p`
@@ -14,4 +19,26 @@ const StyleText = styled.p`
   line-height: 24px;
 `;
 
-export { StyleBudgetCard, StyleText };
+const StyledInputBudget = styled.input`
+  width: 100%;
+  font-size: 20px;
+  font-weight: 500;
+  color: #000000;
+  background-color: transparent;
+  border: none;
+  &::placeholder {
+    color: rgba(255, 255, 255, 0.6);
+  }
+`;
+
+const StyledButtonBudget = styled.button`
+  padding: 5px 20px;
+  border-radius: 10px;
+  background: #ffffff;
+  color: #000000;
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 17px;
+`;
+
+export { StyleBudgetCard, StyleText, StyledInputBudget, StyledButtonBudget };
